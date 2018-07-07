796c0c4c6a2d83d75a0bd8d731d10b6b8fe475f9
Sevdanur GENC <sevdanurgenc@gmail.com>
2018-07-07 19:12:15 +0300

    Create LoggingInterceptorTest.java

---
+++ src/test/java/DenemeAgent/LoggingInterceptorTest.java
@@ -0,0 +1,12 @@
+package DenemeAgent;
+
+import org.junit.Test;
+
+public class LoggingInterceptorTest {
+
+    @Test
+    public void test() {
+        SimpleClass.baslangicMetodu("deneme 01");
+        SimpleClass.bitisMetodu();
+    } 
+}
